2cb1a8084f5ebdc3cb462cedae879135f628a4d9
shehandezen <shehandezen@gmail.com>
2024-08-31 13:38:55 +0530

    chrome extention updated

--- game-chrome-ext/background.ts
+++ game-chrome-ext/background.ts
@@ -32,22 +32,22 @@ export function connectWebSocket() {
         } 
          
         })
-        for await (let tab of tabs) {
+        // for await (let tab of tabs) {
          
-          chrome.debugger.detach({ tabId: tab.id }, () => {
-            console.log(tab.id)
-            if (chrome.runtime.lastError) {
-              console.log("No existing debugger to detach or other error: ", chrome.runtime.lastError.message);
-            } else {
-              console.log('Successfully detached', chrome.runtime.lastError);
-              chrome.tabs.remove(tab.id, () => {
-                console.log(`Closed tab with ID: ${tab.id}`);
-              });
-          } 
+        //   chrome.debugger.detach({ tabId: tab.id }, () => {
+        //     console.log(tab.id)
+        //     if (chrome.runtime.lastError) {
+        //       console.log("No existing debugger to detach or other error: ", chrome.runtime.lastError.message);
+        //     } else {
+        //       console.log('Successfully detached', chrome.runtime.lastError);
+        //       chrome.tabs.remove(tab.id, () => {
+        //         console.log(`Closed tab with ID: ${tab.id}`);
+        //       });
+        //   } 
            
-          })
+        //   })
 
-        }
+        // }
         // tabsList.splice(0, tabsList.length)
 
 
@@ -59,66 +59,68 @@ export function connectWebSocket() {
           let requestId: any
           chrome.debugger.attach({ tabId: tab.id }, "1.3", () => {
             console.log('Debugger attached');
-            chrome.debugger.sendCommand({ tabId: tab.id }, "Network.enable");
-            var start: any
-            var end: any
-            var crash: any
-            var bet = false
-            chrome.debugger.onEvent.addListener((source, method, params: any) => {
-
-
-              var timestampLog = '[' + Date.now() + '] ';
-              if (method === "Network.webSocketFrameReceived") {
-
-                if (requestId != params.requestId) {
-
-                  let payloadString = params.response.payloadData.toString('utf8');
-
-
-                  try {
-                    payloadString = payloadString.replace(/[^\x20-\x7E]/g, '');
-                    const payload = JSON.parse(payloadString);
-                    // console.log(payload)
-
-                    if (payloadString.includes('"target":"OnBets"')) {
-                      if (!bet) {
-                        socket.send(JSON.stringify({ header: 'BET' }))
-                        console.log(timestampLog, ' OnBets Event triggered.')
-                        bet = true
+            chrome.debugger.sendCommand({ tabId: tab.id }, "Network.enable",{}, ()=>{
+              var start: any
+              var end: any
+              var crash: any
+              var bet = false
+              chrome.debugger.onEvent.addListener((source, method, params: any) => {
+                if (source.tabId !== tab.id) return;
+  
+                var timestampLog = '[' + Date.now() + '] ';
+                if (method === "Network.webSocketFrameReceived") {
+  
+                  if (requestId != params.requestId) {
+  
+                    let payloadString = params.response.payloadData.toString('utf8');
+  
+  
+                    try {
+                      payloadString = payloadString.replace(/[^\x20-\x7E]/g, '');
+                      const payload = JSON.parse(payloadString);
+                      // console.log(payload)
+  
+                      if (payloadString.includes('"target":"OnBets"')) {
+                        if (!bet) {
+                          socket.send(JSON.stringify({ header: 'BET' }))
+                          console.log(timestampLog, ' OnBets Event triggered.')
+                          bet = true
+                        }
                       }
+  
+                      if (payloadString.includes('"target":"OnStage"')) {
+                        console.log({ start: start, end: end, odd: crash })
+                        socket.send(JSON.stringify({ header: 'DATA', data: { start: start, end: end, odd: crash } }))
+  
+                      }
+  
+                      if (payloadString.includes('"target":"OnStart"')) {
+                        const { ts } = payload.arguments[0];
+                        start = ts
+                      }
+                      if (payloadString.includes('"type":1,"target":"OnCrash"')) {
+                        bet = false
+                        const { f, ts } = payload.arguments[0];
+                        crash = f
+                        end = ts
+                        console.log(`${timestampLog} ${f}, ${start}, ${ts}`);
+                        socket.send(JSON.stringify({ header: 'CRASH', data: { odd: f } }))
+                      }
+  
+                    } catch (error) {
+                      console.error(timestampLog, 'Error processing WebSocket frame:', error);
                     }
-
-                    if (payloadString.includes('"target":"OnStage"')) {
-                      console.log({ start: start, end: end, odd: crash })
-                      socket.send(JSON.stringify({ header: 'DATA', data: { start: start, end: end, odd: crash } }))
-
-                    }
-
-                    if (payloadString.includes('"target":"OnStart"')) {
-                      const { ts } = payload.arguments[0];
-                      start = ts
-                    }
-                    if (payloadString.includes('"type":1,"target":"OnCrash"')) {
-                      bet = false
-                      const { f, ts } = payload.arguments[0];
-                      crash = f
-                      end = ts
-                      console.log(`${timestampLog} ${f}, ${start}, ${ts}`);
-                      socket.send(JSON.stringify({ header: 'CRASH', data: { odd: f } }))
-                    }
-
-                  } catch (error) {
-                    console.error(timestampLog, 'Error processing WebSocket frame:', error);
                   }
+  
+                } else if (method === "Network.webSocketFrameSent") {
+                  // console.log("WebSocket frame sent: ", params);
                 }
-
-              } else if (method === "Network.webSocketFrameSent") {
-                // console.log("WebSocket frame sent: ", params);
-              }
+              });
             });
-          });
-
-
+  
+  
+            });
+           
         })
       })
 
