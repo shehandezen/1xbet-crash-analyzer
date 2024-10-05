58db498f8f9df622c906dd99a9dcecf88b351dc0
Hetti Arachchige Savindu Shehan <117962951+shehandezen@users.noreply.github.com>
2024-10-05 12:48:36 +0000

    Pending changes exported from your codespace

--- game-server/server.js
+++ game-server/server.js
@@ -33,7 +33,7 @@ const config = {
   timeData: [{ x: 0, y: [0, 0, 0, 0] }],
   hourData: [0],
   MainProfit: [0],
-  profitTarget: 10,
+  profitTarget: 5,
   lossLimit: -600,
   profitLimit: 0,
   predictedCrashPoint: 0,
@@ -348,7 +348,7 @@ const timeDataCollector = async (value) => {
     !(config.lastTime == 0) &&
     parseInt(config.lastRecords.crashData?.timestamp) -
       parseInt(config.lastTime) >
-      1000 * 60 * 60
+      1000 * 60 * 30
   ) {
     let record = {
       x: config.timeData.length + 1,
@@ -490,7 +490,7 @@ const timeDataCollector = async (value) => {
                 status: "Bet stopped",
                 period:
                   parseInt(config.lastTime) +
-                  1000 * 60 * 55 -
+                  1000 * 60 * 30 -
                   parseInt(config.lastRecords.crashData?.timestamp),
               },
             })
@@ -506,13 +506,13 @@ const timeDataCollector = async (value) => {
             );
           });
         }, parseInt(config.lastTime) +
-          1000 * 60 * 55 -
+          1000 * 60 * 30 -
           parseInt(config.lastRecords.crashData?.timestamp));
 
         console.log(
           "set timer to wake  up",
           parseInt(config.lastTime) +
-            1000 * 60 * 55 -
+            1000 * 60 * 30 -
             parseInt(config.lastRecords.crashData?.timestamp)
         );
 
@@ -538,7 +538,7 @@ const timeDataCollector = async (value) => {
 
   if (
     config.timedown.length >= 1 &&
-    config.hourData[config.hourData.length - 1] > config.timedown[0]
+    config.hourData[config.hourData.length - 1] >= config.timedown[0].value
   ) {
     config.timeBet = true;
     console.log("time bet profit");
